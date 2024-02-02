f75611da4daa116b387ee487058e89728cbbfde8
Kareem-Recode <65184465+Kareem-Recode@users.noreply.github.com>
2024-02-02 20:06:17 +0530

    Update Simple.java
    Git's to Jenkins's

--- Simple.java
+++ Simple.java
@@ -2,6 +2,6 @@
 
 class Simple{
 	public static void main (String[] args) {
-	 System.out.println("Welcome to Git's Edureka Training!!!");
+	 System.out.println("Welcome to Jenkins's Edureka Training!!!");
 	}
 }	
